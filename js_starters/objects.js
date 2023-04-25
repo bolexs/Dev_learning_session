b21cab62c1af071f2b4f3b6437fd13ff63e5ff04
bolexs <boluwatufeadeojo@gmail.com>
2023-04-25 22:12:19 +0100

    worked

---
+++ js_starters/objects.js
@@ -0,0 +1,13 @@
+// object are collections of key/value pairs or properties.
+
+
+let person = {
+    firstName: 'sam',
+    lastName: 'folarin'
+};
+
+person.greet = function () {
+    console.log('where is esther');
+}
+
+person.greet();
